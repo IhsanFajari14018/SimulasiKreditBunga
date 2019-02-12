07ad4e7c01c6e6a6d9ffdeb24588a7cb28fa84dc
Ihsan Fajari <ihsan.fajari@gmail.com>
2019-02-12 11:47:06 +0700

    Update class to implement interface Kredit

--- SimulasiKreditEfektif/src/KreditEfektif.java
+++ SimulasiKreditEfektif/src/KreditEfektif.java
@@ -5,15 +5,15 @@
  */
 
 /**
- *
- * @author i14018
+ * Kelas simulasi kredit dengan metode perhitungan bunga efektif.
+ * @author i14018 Ihsan Fajari
  */
-public class KreditEfektif {
+public class KreditEfektif implements Kredit{
     
-    private double pinjaman ;
-    private double sukuBunga;
-    private double tenor;
-    private double biayaAdmin;
+    private final double pinjaman ;
+    private final double sukuBunga;
+    private final double tenor;
+    private final double biayaAdmin;
     
     public KreditEfektif(double p, double sb, int t, double ba){
         this.pinjaman = p;
@@ -23,62 +23,78 @@ public class KreditEfektif {
     }
     
     /**
-     * Menhitung biaya pokok
+     * Menghitung biaya pokok
      * @return value pokok tipe double
      */
-    private double pokok(){
+    @Override
+    public double pokok(){
         double valuePokok;
         valuePokok = this.pinjaman/this.tenor;
         return valuePokok;
     }
     
     /**
-     * Menhitung biaya pokok
+     * Menghitung biaya pokok
+     * @param bulanKe
+     * @param pokok
      * @return value pokok tipe double
      */
-    private double bunga(int bulanKe, double pokok){
+    @Override
+    public double bunga(int bulanKe, double pokok){
         double valueBunga;
         valueBunga = ((this.pinjaman-(bulanKe*pokok))*(this.sukuBunga/100))/12;
         return valueBunga;    
     }
     
     /**
-     * Menghitung biaya pokok
+     * Menghitung biaya cicilan
+     * @param pokok
+     * @param bunga
+     * @param admin
      * @return value pokok tipe double
      */
-    private double cicilan(double pokok, double bunga){
-        return pokok+bunga;
+    @Override
+    public double cicilan(double pokok, double bunga, double admin){
+        return pokok+bunga+admin;
     }
-    
+        
     /**
-     * Menghitung decimal agar output menjadi 2 angka dibelakang koma (,)
-     * @param value
-     * @param places
-     * @return 
+     * Simulator untuk menghitung Kredit Efektif
+     * @return String dengan format output menyerupai tabel
      */
-    private double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
-
-        long factor = (long) Math.pow(10, places);
-        value = value * factor;
-        long tmp = Math.round(value);
-        return (double) tmp / factor;
-    }
-    
+    @Override
     public String simulasi(){
-        String output = "Periode | Angsuran Bunga | Angsuran Pokok | Total Angsuran | Sisa Pinjaman";
+        String output = "Periode | Angsuran Bunga | Angsuran Pokok | Biaya Admin | Total Angsuran | Sisa Pinjaman";
+        output +="\n"+"----------------------------------------------------------------------------------------";
         double tempPokok;
         double tempBunga;
         double tempTotalAngsuran;
         double tempSisaPinjaman = this.pinjaman;
         
+        double totalAngsuranBunga=0.0;
+        double totalAngsuranPokok=0.0;
+        double totalBiayaAdmin=0.0;
+        double totalAngsuran=0.0;
+        
         for (int i = 0; i < this.tenor; i++) {
             tempPokok = this.pokok();                        
             tempBunga = this.bunga(i,tempPokok);            
-            tempTotalAngsuran = this.cicilan(tempPokok,tempBunga);            
-            tempSisaPinjaman = tempSisaPinjaman - tempPokok;            
-            output += "\n"+(i+1)+"       | "+String.format("%.2f",tempBunga)+"     | "+String.format("%.2f",tempPokok)+"    | "+String.format("%.2f",tempTotalAngsuran)+"    | "+String.format("%.2f",tempSisaPinjaman);
+            tempTotalAngsuran = this.cicilan(tempPokok,tempBunga,this.biayaAdmin);            
+            tempSisaPinjaman = tempSisaPinjaman - tempPokok;   
+            
+            totalAngsuranBunga+=tempBunga;
+            totalAngsuranPokok+=tempPokok;
+            totalBiayaAdmin+=this.biayaAdmin;
+            totalAngsuran+=tempTotalAngsuran;
+            
+            output += "\n"+(i+1)+"       | "+String.format("%.2f",tempBunga)+"     | "+String.format("%.2f",tempPokok)+"   | "+String.format("%.2f",this.biayaAdmin)+"    | "+String.format("%.2f",tempTotalAngsuran)+"    | "+String.format("%.2f",tempSisaPinjaman);
         }       
+        output +="\n"+"----------------------------------------------------------------------------------------";
+        output += "\n"+"TOTAL   | "+String.format("%.2f",totalAngsuranBunga)
+                +"    | "+String.format("%.2f",totalAngsuranPokok)
+                +"  | "+String.format("%.2f",totalBiayaAdmin)
+                +"   | "+String.format("%.2f",totalAngsuran)
+                +"   | ";
         
         return output;
     }
